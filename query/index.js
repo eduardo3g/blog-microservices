e0260bb195bbd8298413293f036dbb35b02e19a6
eduardo3g <eduardosbrasil10@gmail.com>
2020-08-19 19:39:38 -0300

    fix: Incorrect event name.

--- query/index.js
+++ query/index.js
@@ -21,7 +21,7 @@ app.post('/events', (req, res) => {
     posts[id] = { id, title, comments: [] };
   }
 
-  if (type === 'Commentreated') {
+  if (type === 'CommentCreated') {
     const { id, content, postId } = data;
 
     const post = posts[postId];
